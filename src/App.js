d3501024cd478b6849d86b2c3043c969749490af
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-04-25 19:28:28 +1000

    create category cards from sanity studio

--- src/App.js
+++ src/App.js
@@ -6,17 +6,31 @@ import About from "./views/About";
 import Foods from "./views/Foods";
 import FilteredFoods from "./views/FilteredFoods";
 
+// components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 // styles
 import "./App.css";
 
 function App() {
 	return (
+		<>
+			{/* Header Area */}
+			<div className="max-w-full">
+					<Header />
+			</div>
 			<Routes>
 				<Route element={<Home />} exact path="/" />
 				<Route element={<About />} path="/About" />
 				<Route element={<Foods />} path="/Foods" />
 				<Route element={<FilteredFoods />} path="/FilteredFoods/:slug" />
 			</Routes>
+			{/* Footer Area */}
+			<div className="max-w-full">
+				<Footer />
+			</div>
+		</>
 	);
 }
 
---
+++ src/components/Categories.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import sanityClient from "../client";
+
+import Category from "./Category";
+
+const Categories = () => {
+	const [categories, setCategories] = useState(null);
+
+	useEffect(() => {
+		sanityClient
+			.fetch(
+				`*[_type == "category"]{
+      title,
+      slug,
+      description,
+      image{
+        asset->{
+          _id,
+          url
+        },
+      },
+      hexCode,
+    }`
+			)
+			.then((data) => setCategories(data))
+			.catch(console.error);
+	}, []);
+
+	return (
+		<div className="">
+			<h3 className="text-3xl font-bold text-center my-10 lg:my-5">
+				All Categories🥘
+			</h3>
+
+			<div className="flex flex-col lg:flex-row lg:justify-center flex-wrap w-full gap-10 my-10">
+				{categories &&
+					categories.map((category) => (
+						<Category key={category._id} category={category} />
+					))}
+			</div>
+		</div>
+	);
+};
+
+export default Categories;
