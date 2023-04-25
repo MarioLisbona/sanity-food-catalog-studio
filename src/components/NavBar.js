7fc002328d0ae9cd1508ed1eae32ebd647c63abd
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-04-26 08:56:33 +1000

    CHange NavLink to Link from react-route-dom

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,41 +1,41 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
 		<nav className="container lg:px-0 px-5 py-2 lg:py-0 lg:w-3/4 w-full mx-auto flex flex-col lg:flex-row justify-between h-20 items-center font-bold">
-			<NavLink to="/">
+			<Link to="/">
 				<p className="text-xl lg:text-2xl">😋Yummy Food's</p>
-			</NavLink>
+			</Link>
 			<div className=" lg:block">
 				<ul className="flex gap-x-20">
 					<li>
-						<NavLink
+						<Link
 							to="/"
 							exact
 							className="nav-link"
 							activeClassName="active-link"
 						>
 							Home
-						</NavLink>
+						</Link>
 					</li>
 					<li>
-						<NavLink
+						<Link
 							to="/foods"
 							className="nav-link"
 							activeClassName="active-link"
 						>
 							Foods
-						</NavLink>
+						</Link>
 					</li>
 					<li>
-						<NavLink
+						<Link
 							to="/about"
 							className="nav-link"
 							activeClassName="active-link"
 						>
 							About
-						</NavLink>
+						</Link>
 					</li>
 				</ul>
 			</div>
